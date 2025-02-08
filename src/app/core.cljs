fbe9f7f9b39745df8b4808fbcda29e52a64d9c77
Edward Paget <ed.paget@gmail.com>
2025-02-08 16:53:48 -0600

    Add google login button

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,6 +1,7 @@
 (ns app.core
   (:require [uix.core :as uix :refer [defui $]]
             [uix.dom]
+            ["@react-oauth/google" :refer [GoogleOAuthProvider GoogleLogin useGoogleOAuth]]
             [app.router :as router]
             [app.card.edit :refer [edit-card]]
             [app.card.reducer :as card-reducer ]
@@ -20,20 +21,25 @@
        ($ edit-card {:card card :update-card-field (card-reducer/update-field-dispatch dispatch-card!)})
        ($ show-card card))))
 
-  (defui app []
-    ($ router/router {:router-store router/router-store}
-       ($ :div.app-container
-          ($ :div.navbar)
-          ($ :div.content
-             ($ router/route {:route-name :home-page}
-                ($ home-page))
-             ($ router/route {:route-name :cards-index}
-                ($ cards-index))
-             ($ router/route {:route-name :cards-new}
-                ($ cards-show))
-             ($ router/route {:route-name :cards-show}
-                ($ cards-show)))
-          ($ :div.footer))))
+(defui app []
+  (let [[oidc-token set-oidc-token!] (uix/use-state "")]
+  ($ router/router {:router-store router/router-store}
+     ($ GoogleOAuthProvider {:client-id "964961527303-t0l0f6a8oa42p8c15928b4f4vavvbj9v.apps.googleusercontent.com"}
+        ($ :div.app-container
+           ($ :div.navbar)
+             ($ :div.content
+                (if-not (empty? oidc-token)
+                  ($ :<>
+                     ($ router/route {:route-name :home-page}
+                        ($ home-page))
+                     ($ router/route {:route-name :cards-index}
+                        ($ cards-index))
+                     ($ router/route {:route-name :cards-new}
+                        ($ cards-show))
+                     ($ router/route {:route-name :cards-show}
+                        ($ cards-show)))
+                  ($ GoogleLogin {:on-success #(set-oidc-token! (aget % "credential")) :on-error #(prn %)})))
+             ($ :div.footer))))))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
--- src/app/server.clj
+++ src/app/server.clj
@@ -1,5 +1,30 @@
 (ns app.server
+  (:require [reitit.ring :as r]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [integrant.core :as ig])
   (:gen-class))
 
-(defn -main [& args]
-  (println "running"))
+(defn ping-handler [_]
+  {:status 200, :body "pong", :headers {"content-type" "text/plain"}})
+
+(defn make-handler
+  []
+  (r/ring-handler
+   (r/router
+    ["/ping" {:get ping-handler}])))
+
+(def config
+  {:adapter/jetty {:handler (ig/ref :handler/router) :port 3000}
+   :handler/router {}})
+
+(defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
+  (run-jetty handler (-> opts (dissoc :handler) (assoc :join? false))))
+
+(defmethod ig/init-key :handler/router [_ {:keys [oauth]}]
+  (make-handler))
+
+(defmethod ig/halt-key! :adapter/jetty [_ server]
+  (.stop server))
+
+(defn -main [& _]
+  (ig/init config))
